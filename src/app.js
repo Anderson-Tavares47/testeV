3aa2c8e80d5ed2ed58a66e88b2166d09be25de9e
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-06-19 20:46:53 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
+// src/app.js
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+const morgan = require('morgan');
+const multer = require('multer');
 
-const uploadRoutes = require('./routes/upload.routes');
+const solicitantesRoutes = require('./routes/solicitantes');
+const usuariosRoutes = require('./routes/usuarios');
+const demandasRoutes = require('./routes/demandas');
 
 const app = express();
+const upload = multer();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
-// Rotas
-app.use('/api', uploadRoutes);
+app.use('/solicitantes', solicitantesRoutes);
+app.use('/usuarios', usuariosRoutes);
+app.use('/demandas', demandasRoutes);
 
 module.exports = app;
